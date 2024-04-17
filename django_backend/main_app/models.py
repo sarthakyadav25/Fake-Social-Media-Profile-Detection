1676991e975151756be33386165f6d09c1d25246
Sarthak Yadav <sarthakvyadav@gmail.com>
2024-04-17 18:47:07 +0530

    Added Loader and Fixed Dashboard

--- django_backend/main_app/models.py
+++ django_backend/main_app/models.py
@@ -13,4 +13,4 @@ class Profile(models.Model):
     timestamp = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
-        return self.username
+        return f"{self.name} searched {self.username}"
